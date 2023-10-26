8dc518832154243318d762adb2793f4e83cb72be
pravin126 <pravinkalokhe5@.com>
2023-10-26 10:38:39 +0530

    Create Model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    booktitle=models.CharField(max_length=50)
+    boodetails=models.CharField(max_length=100)
+    bookauthor=models.CharField(max_length=50)
+    bookimage=models.ImageField(upload_to='images/')
+    bookpath=models.FileField(upload_to='book_path/')
+
+
+    def __str__(self):
+        return self.booktitle
+        return self.boodetails
+        return self.bookauthor
